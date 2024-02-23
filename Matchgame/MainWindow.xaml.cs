94ede1cdae775c0fdc5a4a5c7c342deea81d3665
246749 <246749@usn.no>
2024-02-23 18:25:36 +0100

    Add project files.

---
+++ Matchgame/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Matchgame
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            List<string> animalEmoji = new List<string>()
+            {
+                "🦁","🦁",
+                "🐶","🐶",
+                "🐯","🐯",
+                "🐴","🐴",
+                "🐼","🐼",
+                "🐭","🐭",
+                "🦉","🦉",
+                "🦜","🦜",
+            };
+
+            Random random = new Random();
+
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                textBlock.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
+        }
+    }
+    }
